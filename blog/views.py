f9425487275973df7c30b056fc4e209664ac476c
V0lbD <vova_zl@mail.ru>
2022-07-03 19:20:16 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,5 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 # Create your views here.
